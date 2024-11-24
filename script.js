b58f5994a783a8b30bac5a63469f00c9109bc283
Aditya Kumar <114895058+adityakqumar@users.noreply.github.com>
2024-11-24 17:10:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -11,3 +11,15 @@ document.getElementById('generateButton').addEventListener('click', () => {
     document.getElementById('result').textContent = `Random Number: ${randomNumber}`;
     document.getElementById('result').style.background = 'linear-gradient(45deg, #7e57c2, #42a5f5)';
 });
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("./service-worker.js")
+      .then(registration => {
+        console.log("Service Worker registered with scope:", registration.scope);
+      })
+      .catch(err => {
+        console.log("Service Worker registration failed:", err);
+      });
+  });
+}
